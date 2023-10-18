da677ded08005fea7d0c8efce3cd298869658e23
maria <mjsel.reynolds@ufl.edu>
2023-10-18 16:37:03 -0400

    Add subtract function and print add function of two numbers in main

---
+++ main.py
@@ -0,0 +1,12 @@
+def add(x,y):
+    return x + y
+
+def subtract(x,y):
+    return x - y
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(add(1,2))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
