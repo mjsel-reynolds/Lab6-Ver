d65e4e7f692c306dd70c205f8c57acdf0cc4bc6d
tomczakb <b.tomczak@ufl.edu>
2023-10-24 20:00:13 +0200

    Here are the changes from Benjamin: Added: def decode(new_password) added Added: 1 line of code added in main() to call decode(new_password) Added: Comments to fix encode(user_string) output

--- main.py
+++ main.py
@@ -31,15 +31,28 @@ def encode(user_string):
 			count = 0
 			for num in range(0,2):
 				count += 1
-			digit = count
+			digit = count  # note from Benjamin: the code starting "elif digit > 6:" to here
+		# always results in "2". So an input user_password of 123456789 incorrectly encodes as
+		# 123456222. Instead of "num in range", use "digit" to convert 7, 8,  and 9 to 0,
+		# 1 and 2. "Count" is not needed.
 		# Store encoded password to new variable
 		temp_password += str(digit)
 	return temp_password
 
 
 # Partner: You may write the decoder function here
-def decode():
-	pass
+def decode(new_password):
+	"""# added by Benjamin to decode new_password using the lab's requirement of each digit being
+	shifted up by 3 numbers. This will correctly decode when the encode(user_string) function
+	is fixed.
+	"""
+	decoded = ""
+	for i in new_password:
+		if 3 <= int(i) <= 9:
+			decoded += str(int(i) - 3)
+		if 0 <= int(i) <= 2:
+			decoded += str(int(i) + 7)
+	return decoded
 
 
 # Start of main function
@@ -59,7 +72,7 @@ if __name__ == '__main__':
 			new_password = encode(user_password)
 			print('Your password has been encoded and stored!')
 		elif user_option == 2:
-			pass
+			decoded = decode(new_password)  # added by Benjamin to call decode(new_password)
 		elif user_option == 3:
 			break
 
