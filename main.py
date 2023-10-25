4eb54e11ae826a8bbab32bd4808c7ab1c463cb45
maria <mjsel.reynolds@ufl.edu>
2023-10-24 22:50:01 -0400

    Benjamin added decode() function and implemented it in main(). I fixed my encode() function to encode ints > 6 correctly, and the main function for printing option 2.

--- main.py
+++ main.py
@@ -11,32 +11,35 @@ def print_menu():
 		  '3. Quit\n')
 
 def encode(user_string):
-	# Take in 8-digit password in string format with only integers
-	# Each digit is shifted up by 3 numbers
-	# Stores encoded password to new variable
+	""" Updated to correctly return encoded string that moves
+	each original number decimals string each up by 3
+	:param user_string: password to be encoded
+	:return: encoded password
+	"""
 
 	# Create empty string to store new password
 	temp_password = ''
 
-	# 12345555" -> '45678888'
 	# Iterate through user string
 	for i in user_string:
 		digit = int(i)
 		# Handle digits that will not recount from 0
 		if digit <= 6:
 			digit += 3
+			# Line 28: Change made after Benjamin's push to repository:
+			temp_password += str(digit)
 		# Handles digit that recount from 0 once
 		# they reach 9
 		elif digit > 6:
-			count = 0
-			for num in range(0,2):
-				count += 1
-			digit = count  # note from Benjamin: the code starting "elif digit > 6:" to here
+			# Line 32 - 36: Change made after Benjamin's push to repository:
+			new_digit = 0
+			for i in range(digit + 1):
+				new_digit = int(i) - 7
+			temp_password += str(new_digit)
+			#(digit = count)  # note from Benjamin: the code starting "elif digit > 6:" to here
 		# always results in "2". So an input user_password of 123456789 incorrectly encodes as
 		# 123456222. Instead of "num in range", use "digit" to convert 7, 8,  and 9 to 0,
 		# 1 and 2. "Count" is not needed.
-		# Store encoded password to new variable
-		temp_password += str(digit)
 	return temp_password
 
 
@@ -73,6 +76,8 @@ if __name__ == '__main__':
 			print('Your password has been encoded and stored!')
 		elif user_option == 2:
 			decoded = decode(new_password)  # added by Benjamin to call decode(new_password)
+			# Line 80: Change made after Benjamin's push to repository:
+			print(f'The encoded password is {new_password}, and the original password is {decoded}.')
 		elif user_option == 3:
 			break
 
